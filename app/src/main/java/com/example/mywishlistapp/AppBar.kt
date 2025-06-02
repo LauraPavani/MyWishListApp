57446f8f1ec19e017ee67714dd6832ff8e1b37a8
laura <laura.carolpavani@gmail.com>
2025-06-03 06:19:26 +1000

    Adding the float button

--- app/src/main/java/com/example/mywishlistapp/AppBar.kt
+++ app/src/main/java/com/example/mywishlistapp/AppBar.kt
@@ -21,14 +21,18 @@ fun AppBarView(
     onBackNavClicked: () -> Unit = {}
 ){
     val navigationIcon: (@Composable () -> Unit)? =
-        {
-            IconButton(onClick = {onBackNavClicked() }) {
-                Icon(
-                    imageVector = Icons.Filled.ArrowBack,
-                    tint = Color.White,
-                    contentDescription = null
-                )
+        if(!title.contains("WishList")){
+            {
+                IconButton(onClick = {onBackNavClicked() }) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        tint = Color.White,
+                        contentDescription = null
+                    )
+                }
             }
+        }else{
+            null
         }
 
     TopAppBar(title = {
