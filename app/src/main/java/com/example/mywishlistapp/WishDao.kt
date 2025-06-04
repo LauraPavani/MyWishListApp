fc0038b5e217bf27c72dc9430da5d5d24608443d
laura <laura.carolpavani@gmail.com>
2025-06-04 12:16:31 +1000

    Prepping the ViewModel and understanding Dispatchers and lateinit var

--- app/src/main/java/com/example/mywishlistapp/WishDao.kt
+++ app/src/main/java/com/example/mywishlistapp/WishDao.kt
@@ -12,17 +12,17 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 abstract class WishDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    abstract suspend fun addWish(wishEntity: Wish)
+    abstract suspend fun addAWish(wishEntity: Wish)
 
     //Loads all wishes from the wish table
     @Query("Select * from ``wish-table``")
     abstract fun getAllWishes(): Flow<List<Wish>>
 
     @Update
-    abstract suspend fun updateWish(wishEntity: Wish)
+    abstract suspend fun updateAWish(wishEntity: Wish)
 
     @Delete
-    abstract suspend fun deleteWish(wishEntity: Wish)
+    abstract suspend fun deleteAWish(wishEntity: Wish)
 
     @Query("Select * from ``wish-table`` where id=:id")
     abstract fun getAWishById(id:Long): Flow<Wish>
--- app/src/main/java/com/example/mywishlistapp/WishRepository.kt
+++ app/src/main/java/com/example/mywishlistapp/WishRepository.kt
@@ -3,12 +3,12 @@ package com.example.mywishlistapp
 import com.example.mywishlistapp.data.Wish
 import kotlinx.coroutines.flow.Flow
 
-class WishRepository(private val wishDao: WishDao: WishDao) {
+class WishRepository(private val wishDao: WishDao) {
     suspend fun addAWish(wish: Wish){
         wishDao.addAWish(wish)
     }
 
-    fun getWishes: Flow<List<Wish>> = wishDao.getAllWishes()
+    fun getWishes(): Flow<List<Wish>> = wishDao.getAllWishes()
     fun getAWishById(id: Long) : Flow<Wish>{
         return wishDao.getAWishById(id)
     }
