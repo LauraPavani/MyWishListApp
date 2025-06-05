b856becbbf1389a573822d08bdad89c78a5cda01
laura <laura.carolpavani@gmail.com>
2025-06-05 13:16:05 +1000

    Updating the click on a Wish and see it on the detail view

--- app/src/main/java/com/example/mywishlistapp/HomeView.kt
+++ app/src/main/java/com/example/mywishlistapp/HomeView.kt
@@ -44,7 +44,7 @@ fun HomeView(
                 onClick =  {
                     //Toast.makeText(context,"Float Button Clicked", Toast.LENGTH_LONG).show()
                     //To add Navigation to add screen
-                    navController.navigate(Screen.AddScreen.route)
+                    navController.navigate(Screen.AddScreen.route + "/0L")
 
                 }) {
                 Icon(imageVector = Icons.Default.Add, contentDescription = null)
@@ -58,8 +58,9 @@ fun HomeView(
             .padding(it)){
             items(wishlist.value){
                 wish -> WishItem(wish = wish) {
-
-            }
+                    val id = wish.id
+                    navController.navigate(Screen.AddScreen.route + "/$id")
+                }
             }
         }
     }
